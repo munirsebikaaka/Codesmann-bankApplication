97421d79e25ceaabbc1e8d1c08b35a926e004d4a
munir sebikaaka <munirsebikaaka@gmail.com>
2024-05-01 10:58:57 +0300

    feat: implementted the dates fuctionallity in the application

--- script.js
+++ script.js
@@ -10,6 +10,19 @@ const account1 = {
   movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
   interestRate: 1.2, // %
   pin: 1111,
+
+  movementsDates: [
+    '2019-11-18T21:31:17.178Z',
+    '2019-12-23T07:42:02.383Z',
+    '2020-01-28T09:15:04.904Z',
+    '2020-04-01T10:17:24.185Z',
+    '2020-05-08T14:11:59.604Z',
+    '2024-04-04T17:01:17.194Z',
+    '2024-04-05T23:36:17.929Z',
+    '2024-04-08T10:51:36.790Z',
+  ],
+  currency: 'EUR',
+  locale: 'pt-PT',
 };
 
 const account2 = {
@@ -17,6 +30,19 @@ const account2 = {
   movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
   interestRate: 1.5,
   pin: 2222,
+
+  movementsDates: [
+    '2019-11-01T13:15:33.035Z',
+    '2019-11-30T09:48:16.867Z',
+    '2019-12-25T06:04:23.907Z',
+    '2020-01-25T14:18:46.235Z',
+    '2020-02-05T16:33:06.386Z',
+    '2020-04-10T14:43:26.374Z',
+    '2020-06-25T18:49:59.371Z',
+    '2020-07-26T12:01:20.894Z',
+  ],
+  currency: 'USD',
+  locale: 'en-US',
 };
 
 const accounts = [account1, account2];
@@ -47,14 +73,20 @@ const inputLoanAmount = document.querySelector('.form__input--loan-amount');
 const inputCloseUsername = document.querySelector('.form__input--user');
 const inputClosePin = document.querySelector('.form__input--pin');
 
-function displayMovents(movements) {
-  movements.forEach((mov, i) => {
+function displayMovents(acc) {
+  acc.movements.forEach((mov, i) => {
     const type = mov > 0 ? 'deposit' : 'withdrawal';
+    const date = new Date(acc.movementsDates[i]);
+    const days = String(date.getDate()).padStart(2, 0);
+    const month = String(date.getMonth()).padStart(2, 0);
+    const year = date.getFullYear();
+    const displayDate = `${days}/${month}/${year}`;
     let htmlCode = `
       <div class="movements__row">
           <div class="movements__type movements__type--${type}">${
       i + 1
     } ${type}</div>
+    <div class="movements__date">${displayDate}</div>
           <div class="movements__value">${mov}</div>
         </div>
       `;
@@ -62,27 +94,41 @@ function displayMovents(movements) {
   });
 }
 // displayMovents(account1);
-const calcDisplayBalance = movements => {
-  const balance = movements.reduce((acu, curEL) => acu + curEL);
-  labelBalance.textContent = balance;
+const calcDisplayBalance = acc => {
+  const balance = acc.movements.reduce((acu, curEL) => acu + curEL);
+  labelBalance.textContent = new Intl.NumberFormat(acc.locale, {
+    style: 'currency',
+    currency: acc.currency,
+  }).format(balance);
 };
 // calcDisplayBalance(account1);
-const displaySummerry = movements => {
-  const incomes = movements
+const displaySummerry = acc => {
+  const incomes = acc.movements
     .filter(mov => mov > 0)
     .reduce((acu, curEl) => acu + curEl);
-  labelSumIn.textContent = incomes;
+  labelSumIn.textContent = new Intl.NumberFormat(acc.locale, {
+    style: 'currency',
+    currency: acc.currency,
+  }).format(incomes);
 
-  const withdrawals = movements
+  const withdrawals = acc.movements
     .filter(mov => mov < 0)
     .reduce((acu, curEl) => acu + curEl);
-  labelSumOut.textContent = `${Math.abs(withdrawals)}`;
+  const withs = `${Math.abs(withdrawals)}`;
+  labelSumOut.textContent = new Intl.NumberFormat(acc.locale, {
+    style: 'currency',
+    currency: acc.currency,
+  }).format(withs);
 
-  const interests = movements
+  const interests = acc.movements
     .filter(mov => mov > 0)
     .reduce((acu, curEl) => acu + curEl);
   const ints = (interests / 100) * 1.2;
-  labelSumInterest.textContent = `${Math.trunc(ints)}`;
+  const intes = `${Math.trunc(ints)}`;
+  labelSumInterest.textContent = new Intl.NumberFormat(acc.locale, {
+    style: 'currency',
+    currency: acc.currency,
+  }).format(intes);
 };
 // displaySummerry(account1);
 const calcUserName = acc => {
@@ -96,11 +142,11 @@ const calcUserName = acc => {
 };
 calcUserName(accounts);
 const upDateUI = bigAC => {
-  displayMovents(bigAC.movements);
+  displayMovents(bigAC);
   ////////////////////////////
-  calcDisplayBalance(bigAC.movements);
+  calcDisplayBalance(bigAC);
   ////////////////////////////
-  displaySummerry(bigAC.movements);
+  displaySummerry(bigAC);
 };
 let currentAccout;
 btnLogin.addEventListener('click', e => {
@@ -115,6 +161,11 @@ btnLogin.addEventListener('click', e => {
     }`;
     inputLoginUsername.value = '';
     inputLoginPin.value = '';
+    const now = new Date();
+
+    labelDate.textContent = new Intl.DateTimeFormat(
+      currentAccout.locale
+    ).format(now);
     upDateUI(currentAccout);
   }
 });
@@ -167,3 +218,6 @@ btnClose.addEventListener('click', e => {
 // LECTures
 
 const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+const now = new Date();
+const intenation = new Intl.DateTimeFormat(account1.locale).format(now);
+console.log(intenation);
